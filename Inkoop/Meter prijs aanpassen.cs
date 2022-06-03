de9b0cfe50e04632d0d64f61a970867066a619b5
Machiel van Emden <machiel.vanemden@almacon.nl>
2022-06-03 09:19:21 +0200

    Go

---
+++ Inkoop/Meter prijs aanpassen.cs
@@ -0,0 +1,115 @@
+using ADODB;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Xml;
+using Ridder.Common.ADO;
+using Ridder.Common.Choices;
+using Ridder.Common.Search;
+using System.Linq;
+using Ridder.Recordset.Extensions;
+using System.Windows.Forms;
+using System.Data;
+using Ridder.Common.Script;
+
+public class RidderScript : CommandScript
+{
+	/*
+	
+	Meter prijs aanpassen, het  programma om voor de geselecteerde regels de bruto prijs aan te passen
+	Uit te voeren vanuit een inkooporder op niet ontvangen regels
+	Geschreven door: Machiel R. van Emden mei-2022
+
+	*/
+	private static DialogResult ShowInputDialog(ref decimal input1)
+	{
+
+		System.Drawing.Size size = new System.Drawing.Size(300, 400);
+		Form inputBox = new Form();
+
+		inputBox.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+		inputBox.ClientSize = size;
+		inputBox.Text = "Hastings";
+
+		Button okButton = new Button();
+		okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+		okButton.Name = "Accept";
+		okButton.Text = "&OK";
+		okButton.Size = new System.Drawing.Size(75, 25);
+		okButton.Location = new System.Drawing.Point(5, 10);
+		inputBox.Controls.Add(okButton);
+
+		Button cancelButton = new Button();
+		cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+		cancelButton.Name = "ABORT";
+		cancelButton.Text = "&Cancel";
+		cancelButton.Size = new System.Drawing.Size(75, 25);
+		cancelButton.Location = new System.Drawing.Point(100, 10);
+		inputBox.Controls.Add(cancelButton);
+
+
+		//groep prijs
+		GroupBox groepprijs = new GroupBox();
+		groepprijs.Size = new System.Drawing.Size(180, 60);
+		groepprijs.Location = new System.Drawing.Point(10, 75);
+		groepprijs.Text = "Prijs / meter";
+
+		System.Windows.Forms.NumericUpDown textBox1 = new NumericUpDown();
+		textBox1.Size = new System.Drawing.Size(100, 25);
+		textBox1.Location = new System.Drawing.Point(5, 25);
+		textBox1.Value = input1;
+		textBox1.Minimum = 0;
+		textBox1.Maximum = 1500000;
+		textBox1.DecimalPlaces = 2;
+		groepprijs.Controls.Add(textBox1);
+
+		inputBox.Controls.Add(groepprijs);
+
+
+
+		inputBox.AcceptButton = okButton;
+		inputBox.CancelButton = cancelButton;
+
+
+		DialogResult result = inputBox.ShowDialog();
+
+		input1 = textBox1.Value;
+
+
+
+
+		return result;
+	}
+
+	public void Execute()
+	{
+
+		decimal input1 = 1;
+
+		ShowInputDialog(ref input1);
+
+		IRecord[] records = this.FormDataAwareFunctions.GetSelectedRecords();
+
+		if (records.Length == 0)
+			return;
+
+		foreach (IRecord record in records)
+		{
+			ScriptRecordset rsItem = this.GetRecordset("R_PURCHASEORDERDETAILITEM", "", "PK_R_PURCHASEORDERDETAILITEM = " + (int)record.GetPrimaryKeyValue(), "");
+			rsItem.MoveFirst();
+
+			rsItem.UseDataChanges = true;
+
+
+			rsItem.Fields["GROSSPURCHASEPRICE"].Value = input1;
+
+			rsItem.Update();
+
+
+
+		}
+	}
+
+
+}
---
+++ Productie/Voorraad reservering verwijderen.cs
@@ -0,0 +1,67 @@
+using ADODB;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Xml;
+using Ridder.Common.ADO;
+using Ridder.Common.Choices;
+using Ridder.Common.Search;
+using System.Linq;
+using Ridder.Recordset.Extensions;
+using System.Windows.Forms;
+using System.Data;
+using Ridder.Common.Script;
+
+public class RidderScript : CommandScript
+{
+    /*
+	
+	Voorraad reservering verwijderen, het  programma om een reservering van een voorraadhoudende bonregel artikel te verwijderen
+	Uit te voeren vanuit een bonregel artikel welke niet verder mag zijn dan gereserveerd
+	Geschreven door: Machiel R. van Emden mei-2022
+
+	*/
+
+	public void Execute()
+	{
+
+		IRecord[] records = this.FormDataAwareFunctions.GetSelectedRecords();
+
+		if (records.Length == 0)
+			return;
+
+
+		foreach (IRecord record in records)
+		{
+			ScriptRecordset rsBonregel = this.GetRecordset("R_JOBORDERDETAILITEM", "", "PK_R_JOBORDERDETAILITEM = " + (int)record.GetPrimaryKeyValue(), "");
+			rsBonregel.MoveFirst();
+			//		rsBonregel.UseDataChanges = true;
+
+			string ItemCode = rsBonregel.Fields["FK_ITEM"].Value.ToString();
+			string omschrijving = rsBonregel.Fields["DESCRIPTION"].Value.ToString();
+			double aantal = Convert.ToDouble(rsBonregel.Fields["QUANTITY"].Value.ToString());
+			double lengte = Convert.ToDouble(rsBonregel.Fields["LENGTH"].Value.ToString());
+			int bon = Convert.ToInt32(rsBonregel.Fields["FK_JOBORDER"].Value.ToString());
+
+			rsBonregel.Delete();
+
+			ScriptRecordset rsJoborderItem = this.GetRecordset("R_JOBORDERDETAILITEM", "", "PK_R_JOBORDERDETAILITEM= -1", "");
+			rsJoborderItem.UseDataChanges = true;
+			rsJoborderItem.AddNew();
+
+			rsJoborderItem.Fields["FK_JOBORDER"].Value = bon;
+			rsJoborderItem.Fields["FK_ITEM"].Value = Convert.ToInt32(ItemCode);
+			rsJoborderItem.Fields["DESCRIPTION"].Value = omschrijving;
+			rsJoborderItem.Fields["QUANTITY"].Value = aantal;
+			rsJoborderItem.Fields["LENGTH"].Value = lengte;
+
+			rsJoborderItem.Update();
+
+		}
+
+		this.FormDataAwareFunctions.Refres();
+
+
+	}
+}
