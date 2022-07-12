2d6f4f062dac0bbfc66616cc8cd86c3e40325342
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-07-12 16:44:14 +0200

    Go

--- Productie/artikelen combo.cs
+++ Productie/artikelen combo.cs
@@ -19,16 +19,16 @@ public class RidderScript : CommandScript
 	{
 		var BonRID = this.FormDataAwareFunctions.CurrentRecord.GetPrimaryKeyValue();
 
-		ScriptRecordset rsBon = this.GetRecordset("R_JOBORDERDETAILITEM", "", string.Format("PK_R_JOBORDERDETAILITEM = '{0}'", BonRID.ToString()), "");
+		ScriptRecordset rsBon = this.GetRecordset("R_JOBORDER", "", string.Format("PK_R_JOBORDER = '{0}'", BonRID.ToString()), "");
 		rsBon.MoveFirst();
 
-		string BonID = rsBon.Fields["FK_JOBORDER"].Value.ToString();
+		string BonID = rsBon.Fields["PK_R_JOBORDER"].Value.ToString();
 
-		ScriptRecordset Bonnen = this.GetRecordset("R_JOBORDERDETAILITEM", "FK_ITEM", string.Format("FK_JOBORDER = '{0}'", BonID), "PK_R_JOBORDERDETAILITEM");
-		ScriptRecordset Bonnen1 = this.GetRecordset("R_JOBORDERDETAILITEM", "FK_ITEM", string.Format("FK_JOBORDER = '{0}'", BonID), "PK_R_JOBORDERDETAILITEM");
+		ScriptRecordset Bonnen = this.GetRecordset("R_JOBORDERDETAILITEM", "", string.Format("FK_JOBORDER = '{0}'", BonID), "PK_R_JOBORDERDETAILITEM");
+		ScriptRecordset Bonnen1 = this.GetRecordset("R_JOBORDERDETAILITEM", "", string.Format("FK_JOBORDER = '{0}'", BonID), "PK_R_JOBORDERDETAILITEM");
 		int aantal = Bonnen.RecordCount;
 		int aantal1 = Bonnen1.RecordCount;
-		MessageBox.Show(aantal.ToString()+" / "+aantal1.ToString());
+		//MessageBox.Show(aantal.ToString()+" / "+aantal1.ToString());
 		
 		Bonnen.MoveFirst();
 		
@@ -38,15 +38,24 @@ public class RidderScript : CommandScript
 		while (i < aantal)
 		{
 			string nummer = Bonnen.Fields["FK_ITEM"].Value.ToString();
+			string regel = Bonnen.Fields["PK_R_JOBORDERDETAILITEM"].Value.ToString();
+			string lengte = Bonnen.Fields["LENGTH"].Value.ToString();
+			
 			Bonnen1.MoveFirst();
-			Bonnen1.MoveNext();
+			//Bonnen1.MoveNext();
 
-			while (j < aantal)
+			while (j < aantal1)
 			{
+				//MessageBox.Show("Test"+"-"+j.ToString());
+
 				string nummer1 = Bonnen1.Fields["FK_ITEM"].Value.ToString();
-				if (nummer == nummer1)
+				string regel1 = Bonnen1.Fields["PK_R_JOBORDERDETAILITEM"].Value.ToString();
+				string lengte1 = Bonnen1.Fields["LENGTH"].Value.ToString();
+				if (	nummer == nummer1 && 
+						regel != regel1 &&
+						lengte == lengte1)
 				{
-					MessageBox.Show(nummer1);
+					MessageBox.Show("Gelijk");
 				}
 				Bonnen1.MoveNext();
 
@@ -54,8 +63,11 @@ public class RidderScript : CommandScript
 
 			}
 
+			MessageBox.Show("Loop -"+i.ToString());
+
 			Bonnen.MoveNext();
 			i++;
+			j=0;
 		}		
 
 	}
