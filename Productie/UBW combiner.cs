56b7da2e8e2df42ec7eee624bdd8f89361d08e47
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-11-30 09:21:34 +0100

    Go

---
+++ Productie/UBW combiner.cs
@@ -0,0 +1,26 @@
+using ADODB;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Xml;
+using Ridder.Common.ADO;
+using Ridder.Common.Choices;
+using Ridder.Common.Search;
+using System.Linq;
+using Ridder.Recordset.Extensions;
+using System.Windows.Forms;
+using System.Data;
+using Ridder.Common.Script;
+
+public class RidderScript : CommandScript
+{
+	public void Execute()
+	{
+
+
+        
+
+
+    }
+}
