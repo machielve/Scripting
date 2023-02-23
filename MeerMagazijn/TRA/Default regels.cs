6260f2d858d91ca211b7c1d099bf3c7fc0d37077
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2023-02-23 10:33:10 +0100

    Go

--- MeerMagazijn/TRA/Default regels.cs
+++ MeerMagazijn/TRA/Default regels.cs
@@ -12,106 +12,131 @@ using Ridder.Recordset.Extensions;
 using System.Windows.Forms;
 using System.Data;
 using Ridder.Common.Script;
+using System.IO;
+using Microsoft.VisualBasic;
 
 public class RidderScript : CommandScript
 {
 	public void Execute()
 	{
 
-		var ordernummer = this.FormDataAwareFunctions.CurrentRecord.GetPrimaryKeyValue();
+		/*
 
-        int taaknummer;
-        int risiconummer;
-        int maatregelnummer;
-        int actiedoor = 3;          //monteur
-        
-        
-        //regel 1
-        taaknummer = 1;             //werken op hoogte          
-        risiconummer = 1;           //valgevaar
-        maatregelnummer = 1;        //gebruik harnas 
-        Regelvuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
+		Default TRA regels, het  programma om de default TRA regels op een Meer magazijn order toe te voegen
+		Uit te voeren vanuit een order
+		Geschreven door: Machiel R. van Emden jan-2023
 
-        //regel 2
-        taaknummer = 1;             //werken op hoogte
-        risiconummer = 2;           //onder elkaar werken
-        maatregelnummer = 2;        //draag helm
-        Regelvuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
+		*/
 
-        //regel 3
-        taaknummer = 1;             //fysiek zwaar werk
-        risiconummer = 2;           //fysieke belasting tillen
-        maatregelnummer = 2;        //voorlichting hulpmiddelen
-        Regelvuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
+		var ordernummer1 = this.FormDataAwareFunctions.CurrentRecord.GetPrimaryKeyValue().ToString();
 
-        //regel 4
-        taaknummer = 1;             //fysiek zwaar werk
-        risiconummer = 2;           //werkhouding
-        maatregelnummer = 2;        //voorlichting tilinstructie
-        Regelvuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
+		int ordernummer = Convert.ToInt32(ordernummer1);
+		int taaknummer;
+		int risiconummer;
+		int maatregelnummer;
+		int actiedoor = 2;          //monteur
 
-        //regel 5
-        taaknummer = 1;             //vallen / struikelen / beklemming
-        risiconummer = 2;           //werken op hoogte
-        maatregelnummer = 2;        //gebruik valharnas
-        Regelvuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
+		ScriptRecordset rsTRARegels = this.GetRecordset("U_TRAREGELS", "", "FK_ORDER = " + ordernummer1, "");
+		rsTRARegels.MoveFirst();
 
-        //regel 6
-        taaknummer = 1;             //vallen van voorwerpen
-        risiconummer = 2;           //onder, boven elkaar werken
-        maatregelnummer = 2;        //helm
-        Regelvuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
+		int regelaantal = rsTRARegels.RecordCount;
 
-        //regel 7
-        taaknummer = 1;             //vallen van voorwerpen
-        risiconummer = 2;           //onder, boven elkaar werken
-        maatregelnummer = 2;        //geen los gereedschap
-        Regelvuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
+		if (regelaantal > 0)
 
-        //regel 8
-        taaknummer = 1;             //verkeer
-        risiconummer = 2;           //intern transport
-        maatregelnummer = 2;        //fysieke scheiding
-        Regelvuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
+		{
+			MessageBox.Show("TRA regels al ingevuld, verwijder eerst bestaande regels.","Error");
+			return;
+		}
 
-        //regel 9
-        taaknummer = 1;             //geluid
-        risiconummer = 2;           //geluidsniveau
-        maatregelnummer = 2;        //gehoorbescherming
-        Regelvuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
+		else
+		{
+			//regel 1
+			taaknummer = 1;             //werken op hoogte          
+			risiconummer = 1;           //valgevaar
+			maatregelnummer = 35;        //gebruik harnas 
+			RegelVuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
 
-        //regel 10
-        taaknummer = 1;             //ander werk boven/naast/onder
-        risiconummer = 2;           //vallen voorwerpen
-        maatregelnummer = 2;        //helm
-        Regelvuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
+			//regel 2
+			taaknummer = 1;             //werken op hoogte
+			risiconummer = 38;           //onder elkaar werken
+			maatregelnummer = 37;        //draag helm
+			RegelVuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
 
-        //regel 11
-        taaknummer = 1;             //hijsen
-        risiconummer = 2;           //communicatie
-        maatregelnummer = 2;        //communicatie
-        Regelvuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
+			//regel 3
+			taaknummer = 13;             //fysiek zwaar werk
+			risiconummer = 63;           //fysieke belasting tillen
+			maatregelnummer = 61;        //voorlichting hulpmiddelen
+			RegelVuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
 
-        MessageBox.show("Default regels op TRA toegevoegd.");
-        
+			//regel 4
+			taaknummer = 13;             //fysiek zwaar werk
+			risiconummer = 62;           //werkhouding
+			maatregelnummer = 60;        //voorlichting tilinstructie
+			RegelVuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
 
+			//regel 5
+			taaknummer = 17;             //vallen / struikelen / beklemming
+			risiconummer = 86;           //werken op hoogte
+			maatregelnummer = 83;        //gebruik valharnas
+			RegelVuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
+
+			//regel 6
+			taaknummer = 18;             //vallen van voorwerpen
+			risiconummer = 88;           //onder, boven elkaar werken
+			maatregelnummer = 85;        //helm
+			RegelVuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
+
+			//regel 7
+			taaknummer = 18;             //vallen van voorwerpen
+			risiconummer = 88;           //onder, boven elkaar werken
+			maatregelnummer = 1;        //geen los gereedschap
+			RegelVuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
+
+			//regel 8
+			taaknummer = 19;             //verkeer
+			risiconummer = 94;           //intern transport
+			maatregelnummer = 90;        //fysieke scheiding
+			RegelVuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
+
+			//regel 9
+			taaknummer = 22;             //geluid
+			risiconummer = 107;           //geluidsniveau
+			maatregelnummer = 103;        //gehoorbescherming
+			RegelVuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
+
+			//regel 10
+			taaknummer = 29;             //ander werk boven/naast/onder
+			risiconummer = 148;           //vallen voorwerpen
+			maatregelnummer = 148;        //helm
+			RegelVuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
+
+			//regel 11
+			taaknummer = 12;             //hijsen
+			risiconummer = 58;           //communicatie
+			maatregelnummer = 56;        //communicatie
+			RegelVuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
+
+			MessageBox.Show("Default regels op TRA toegevoegd.","Default TRA regels");
+
+		}
 	}
 
 
 
-    public void RegelVuller(ref int ordernummer, ref int taaknummer, ref int risiconummer, ref int maatregelnummer)
-    {
-        ScriptRecordset rsTRAregel = this.GetRecordset("U_TRAREGELS", "", "U_TRAREGELS = -1 ", "");
-		rsTRAregel.MoveFirst();
-        rsTRAregel.AddNew();
 
-        rsTRAregel.Fields["FK_ORDER"].Value = ordernummer;
-        rsTRAregel.Fields["FK_TRATAKEN"].Value = taaknummer;
-        rsTRAregel.Fields["FK_TRARISICO"].Value = risiconummer;
-        rsTRAregel.Fields["FK_TRAMAATREGEL"].Value = maatregelnummer;        
-        rsTRAregel.Fields["TRAACTION"].Value = actiedoor;
+	public void RegelVuller(ref int ordernummer, ref int taaknummer, ref int risiconummer, ref int maatregelnummer, ref int actiedoor)
+	{
+		ScriptRecordset rsTRAregel = this.GetRecordset("U_TRAREGELS", "", "PK_U_TRAREGELS = -1 ", "");
+		rsTRAregel.MoveFirst();
+		rsTRAregel.AddNew();
+
+		rsTRAregel.Fields["FK_ORDER"].Value = ordernummer;
+		rsTRAregel.Fields["FK_TRATAKEN"].Value = taaknummer;
+		rsTRAregel.Fields["FK_TRARISICOS"].Value = risiconummer;
+		rsTRAregel.Fields["FK_MAATREGELEN"].Value = maatregelnummer;
+		rsTRAregel.Fields["TRAACTIES"].Value = actiedoor;
 
 		rsTRAregel.Update();
 
-    }
+	}
 }
