519b498b035f05995e63c2b16a673e937a005319
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2023-03-29 14:01:41 +0200

    Go

--- Verkoopfactuur/netto termijn controle.cs
+++ Verkoopfactuur/netto termijn controle.cs
@@ -7,32 +7,52 @@ public class RidderScript : ConditionalFormatScript
 {
 	public void Execute()
 	{
-		if (FindItem("TCR").Value.ToString()
-
-		== "18" && //BITO-DE_3 regel 1
+	    //BITO-DE_3 regel 1	
+        if (FindItem("TCR").Value.ToString() == "18" && 
 		this.Item.Value.ToInteger()!=10000)
-
 		{
 			this.Item.Control.SetStrikeOut(true);
 			this.Item.Control.SetBold(true);
 			this.Item.Control.SetItalic(true);
 			this.Item.Control.SetBackgroundColor(Color.Yellow);
-
 		}
 
-		if (FindItem("TCR").Value.ToString()
-
-		== "20" && //BITO-DE_3 regel 3
+        //BITO-DE_3 regel 3
+		if (FindItem("TCR").Value.ToString() == "20" && 
 		FindItem("TP").Value.ToDouble()<0.261 &&
 		FindItem("TP").Value.ToDouble()>0.259)
-
 		{
 			this.Item.Control.SetStrikeOut(true);
 			this.Item.Control.SetBold(true);
 			this.Item.Control.SetItalic(true);
 			this.Item.Control.SetBackgroundColor(Color.Yellow);
-
 		}
+
+
+
+
+        //BRNZEEL_3 regel 1	
+        if (FindItem("TCR").Value.ToString() == "18" && 
+		this.Item.Value.ToInteger()!=10000)
+		{
+			this.Item.Control.SetStrikeOut(true);
+			this.Item.Control.SetBold(true);
+			this.Item.Control.SetItalic(true);
+			this.Item.Control.SetBackgroundColor(Color.Yellow);
+		}
+
+        //BRNZEEL_3 regel 3
+		if (FindItem("TCR").Value.ToString() == "20" && 
+		FindItem("TP").Value.ToDouble()<0.451 &&
+		FindItem("TP").Value.ToDouble()>0.449)
+		{
+			this.Item.Control.SetStrikeOut(true);
+			this.Item.Control.SetBold(true);
+			this.Item.Control.SetItalic(true);
+			this.Item.Control.SetBackgroundColor(Color.Yellow);
+		}
+
+
 
 	}
 }
