366c9286b0f4c65085f4f1ec6b788f49707812e4
Machiel van Emden <machielve@gmail.com>
2023-10-24 20:45:18 +0200

    Go

---
+++ Inkoop/De Ruiter portaal.cs
@@ -0,0 +1,39 @@
+using ADODB;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Xml;
+using Ridder.Common.ADO;
+using Ridder.Common.Choices;
+using Ridder.Common.Search;
+using System.Linq;
+using Ridder.Recordset.Extensions;
+using System.Windows.Forms;
+using System.Data;
+using Ridder.Common.Script;
+
+public class RidderScript : CommandScript
+{
+	/*
+	
+	De ruiter portaal, het  programma om een ridder inkoop order door te schuiven naar een inkoop via het de ruiter portaal
+	Uit te voeren vanuit een inkooporder met de status nieuw
+	Geschreven door: Machiel R. van Emden oktober-2023
+
+	*/
+	
+	public void Execute()
+	{
+
+
+        
+
+
+
+    }
+
+
+	// M.R.v.E - 2023
+
+}
