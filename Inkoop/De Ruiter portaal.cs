ad6a1fe399721fefa2adf6fb7a0906b4212ff832
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2023-10-25 16:36:21 +0200

    Go

--- Inkoop/De Ruiter portaal.cs
+++ Inkoop/De Ruiter portaal.cs
@@ -14,39 +14,110 @@ using System.Data;
 using Ridder.Common.Script;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
 
 public class RidderScript : CommandScript
 {
 	/*
-	
-	De ruiter portaal, het  programma om een ridder inkoop order door te schuiven naar een inkoop via het de ruiter portaal
-	Uit te voeren vanuit een inkooporder met de status nieuw
+	de ruiter portaal, het  programma om een Ridder inkooporder door te zetten naar het online portaal van de ruiter transport.
+	Uit te voeren vanuit een inkooporder met de status nieuw en de bestelwijze op webshop
 	Geschreven door: Machiel R. van Emden oktober-2023
-
 	*/
+	
+
 	
 	public void Execute()
 	{
 
-        string website = "https://portal.deruitertransportbv.nl/Portal4uClient/Login.aspx";
 
-        string user = "info@almacon.nl";
+		Task.Run(async () =>
+		{
+			await LoginAsync();
+		}).Wait();
 
-        string password = "2665NN"; //nog aanpassen
-
-
-        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(website);
-        CookieContainer cookies = new CookieContainer();
+		//	MessageBox.Show("Synchronous code continues here.");
 
 
 
+		MessageBox.Show("Klaar.");
+	}
+
+	public static async Task LoginAsync()
+	{
+		// Create an HttpClientHandler with a CookieContainer to store cookies
+		var handler = new HttpClientHandler
+		{
+			UseCookies = true,
+			CookieContainer = new CookieContainer(),
+		};
+
+		// Create an HttpClient with the handler
+		var httpClient = new HttpClient(handler);
+
+		// Set the base address
+		httpClient.BaseAddress = new Uri("https://portal.deruitertransportbv.nl/Portal4uClient/");
+
+		// Define the login URL and form data
+		string loginUrl = "Login.aspx";
+		var loginData = new FormUrlEncodedContent(new[]
+		{
+			new KeyValuePair<string, string>("tbUsername", "info@almacon.nl"),
+			new KeyValuePair<string, string>("tbPassword", "2665NE"),
+		});
+
+		// Send the login POST request
+		HttpResponseMessage loginResponse = await httpClient.PostAsync(loginUrl, loginData);
 
 
 
+		if (loginResponse.IsSuccessStatusCode)
+		{
+			//	MessageBox.Show("Login successful!");
 
-    }
+			// You can now use the same HttpClient to make further requests with the established session.
+			string NewTransport = "Form.aspx"; //invul scherm
+
+			var TransportData = new FormUrlEncodedContent(new[]
+			{
+				new KeyValuePair<string, string>("Textfield2", "Inkoopordernummer"),
+				new KeyValuePair<string, string>("Textfield5", "Ophaaladres"), 
+			});
 
 
+			HttpResponseMessage protectedPageResponse = await httpClient.GetAsync(NewTransport);
+			
+			
+			if (protectedPageResponse.IsSuccessStatusCode)
+			{
+				MessageBox.Show(protectedPageResponse.ToString());
+				
+				HttpResponseMessage NewTransportResponse = await httpClient.PostAsync(NewTransport, TransportData);
+				
+				if (NewTransportResponse.IsSuccessStatusCode)
+				{
+					MessageBox.Show("yeah");
+				}
+				
+				else MessageBox.Show("bummer");
+				
+				
+			}
+			else
+			{
+				MessageBox.Show("Failed to access the protected page.");
+			}
+		}
+		else
+		{
+			MessageBox.Show("Login failed.");
+		}
+
+
+
+	}
+	
 	// M.R.v.E - 2023
-
 }
