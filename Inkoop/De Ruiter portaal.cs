7ff652f857b8aa9b0203e4e138c2ad03a9835bcc
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2023-10-26 16:29:19 +0200

    Go

--- Inkoop/De Ruiter portaal.cs
+++ Inkoop/De Ruiter portaal.cs
@@ -31,8 +31,6 @@ public class RidderScript : CommandScript
 	
 	public void Execute()
 	{
-
-
 		Task.Run(async () =>
 		{
 			await LoginAsync();
@@ -43,11 +41,14 @@ public class RidderScript : CommandScript
 
 	public static async Task LoginAsync()
 	{
+		RidderScript instance = new RidderScript();		
+		
 		// Create an HttpClientHandler with a CookieContainer to store cookies
+		CookieContainer cookieContainer = new CookieContainer();
 		var handler = new HttpClientHandler
 		{
 			UseCookies = true,
-			CookieContainer = new CookieContainer(),
+			CookieContainer = cookieContainer,
 		};
 
 		// Create an HttpClient with the handler
@@ -57,24 +58,84 @@ public class RidderScript : CommandScript
 		string loginUrl = "https://portal.deruitertransportbv.nl/Portal4uClient/Login.aspx";
 		var loginData = new FormUrlEncodedContent(new[]
 		{
-			new KeyValuePair<string, string>("tbUsername", "info@almacon.nl"),
-			new KeyValuePair<string, string>("tbPassword", "***"),
+			new KeyValuePair<string, string>("tbUsername", 		"info@almacon.nl"),
+			new KeyValuePair<string, string>("tbPassword", 		"***"),
 		});
 
 		// Send the login POST request
 		HttpResponseMessage loginResponse = await httpClient.PostAsync(loginUrl, loginData);
+		
+		
+		
+		
+		/*
+		
+		//check all the current cookies
+		MessageBox.Show(loginResponse.ToString());
+
+		CookieCollection cookies = cookieContainer.GetCookies(new Uri("https://portal.deruitertransportbv.nl"));
+
+		foreach (Cookie cookie in cookies)
+		{
+			MessageBox.Show("Cookie Name: " + cookie.Name);
+			MessageBox.Show("Cookie Value: " + cookie.Value);
+			MessageBox.Show("Domain: " + cookie.Domain);
+			MessageBox.Show("Path: " + cookie.Path);
+			MessageBox.Show("Secure: " + cookie.Secure);
+			MessageBox.Show("Expires: " + cookie.Expires);
+		}
+		
+		
+		*/
+		
+		
+		
 
 		if (loginResponse.IsSuccessStatusCode)
 		{
-			//	MessageBox.Show("Login successful!");
-
 			// You can now use the same HttpClient to make further requests with the established session.
 			string NewTransport = "https://portal.deruitertransportbv.nl/Portal4uClient/Form.aspx?PageId=1&GroupId=2&SubGroupId=6"; //invul scherm
 
+			string inkoopnummer = "";
+			
+			string LaadDatum = "" ;
+			string LaadNaam = "" ; 
+			string LaadAdres = "" ;
+			string LaadPostcode = "" ;
+			string LaadPlaats = "" ;
+			string LaadLand = "" ;
+			string LaadContact = "" ;
+			string LaadTelefoon = "" ;
+			
+			string LosDatum = "" ;
+			string LosNaam = "";
+			string LosAdres = "";
+			string LosPostcode = "";
+			string LosPlaats = "";
+			string LosLand = "";
+			string LosContact = "";
+			string LosTelefoon = "";
+
+			string Opmerkingen = "";
+			
+
+			instance.InkoopData(		ref inkoopnummer, 
+										ref LaadDatum, ref LaadNaam, ref LaadAdres, ref LaadPostcode, ref LaadPlaats, ref LaadLand, ref LaadContact, ref LaadTelefoon,
+										ref LosDatum, ref LosNaam, ref LosAdres, ref LosPostcode, ref LosPlaats, ref LosLand, ref LosContact, ref LosTelefoon,
+										ref Opmerkingen);
+
+		
+			
+		
+			
+			
+			
+			
+			
 			var TransportData = new FormUrlEncodedContent(new[]
 			{
-				new KeyValuePair<string, string>("Textfield2", "Inkoopordernummer"),
-				new KeyValuePair<string, string>("Textfield5", "OphaaladresNaam"), 
+				new KeyValuePair<string, string>("ctl00$MainContentHolder$Textfield2", 		inkoopnummer),
+				new KeyValuePair<string, string>("ctl00$MainContentHolder$Textfield5", 		LaadNaam), 
 			});
 
 
@@ -82,14 +143,11 @@ public class RidderScript : CommandScript
 			
 			
 			
-			
-			
-			
-			
-			
 			if (protectedPageResponse.IsSuccessStatusCode)
 			{
 				MessageBox.Show(protectedPageResponse.ToString());
+				
+				/*
 				
 				HttpResponseMessage NewTransportResponse = await httpClient.PostAsync(NewTransport, TransportData);
 				
@@ -99,6 +157,8 @@ public class RidderScript : CommandScript
 				}
 				
 				else MessageBox.Show("Cannot send the data.");
+
+				*/
 				
 				
 			}
@@ -115,6 +175,42 @@ public class RidderScript : CommandScript
 
 
 	}
+
+	public void InkoopData(	ref string inkoopnummer, 
+							ref string LaadDatum, ref string LaadNaam, ref string LaadAdres, ref string LaadPostcode, 
+							ref string LaadPlaats, ref string LaadLand, ref string LaadContact, ref string LaadTelefoon,
+							ref string LosDatum, ref string LosNaam, ref string LosAdres, ref string LosPostcode,
+							ref string LosPlaats, ref string LosLand, ref string LosContact, ref string LosTelefoon,
+							ref string Opmerkingen)
+	{
+		inkoopnummer = "check";
+		
+		LaadDatum = "01-01-2025";
+		LaadNaam = "Almacon ";
+		LaadAdres = "Kristalstraat 36";
+		LaadPostcode = "2665NE";
+		LaadPlaats = "Bleiswijk" ;
+		LaadLand = "Nederland" ;
+		LaadContact = "Erik";
+		LaadTelefoon = "1234";
+
+		LosDatum = "01-02-2025";
+		LosNaam = "Almacon ";
+		LosAdres = "Kristalstraat 36";
+		LosPostcode = "2665NE";
+		LosPlaats = "Bleiswijk";
+		LosLand = "Nederland";
+		LosContact = "Erik";
+		LosTelefoon = "1234";
+
+		Opmerkingen = "Tralala";
+		
+	}
+	
+	
+	
+	
+	
 	
 	// M.R.v.E - 2023
 }
