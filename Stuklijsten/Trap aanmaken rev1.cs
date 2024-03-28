aca280baa6b3ba107178af9fc3ce4dd34513f532
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2024-03-28 15:16:27 +0100

    Update Trap aanmaken rev1.cs

--- Stuklijsten/Trap aanmaken rev1.cs
+++ Stuklijsten/Trap aanmaken rev1.cs
@@ -24,17 +24,21 @@ public class RidderScript : CommandScript
 
 	*/
 
-	private static DialogResult ShowInputDialog(ref string input, ref decimal input1, ref string input2, 
-												ref bool rb10, ref bool rb11, ref bool rb20, ref bool rb21,
+	private static DialogResult ShowInputDialog(ref string input, ref decimal input1, ref string input2,
+												ref bool rb10, ref bool rb11, ref bool rb12, ref string input10, ref string input11,
+												ref bool rb20, ref bool rb21,
+												ref bool rb30, ref bool rb31, ref string input30,
+												ref bool rb40, ref bool rb41, ref string input40,
 												ref bool rb0, ref bool rb1, ref bool rb2, ref bool rb3, ref bool rb4, ref bool rb5, ref bool rb6, ref bool rb7)
 	{
 
-		System.Drawing.Size size = new System.Drawing.Size(450, 700);
+		System.Drawing.Size size = new System.Drawing.Size(450, 800);
 		Form inputBox = new Form();
 
 		inputBox.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 		inputBox.ClientSize = size;
 		inputBox.Text = "Agincourt";
+		inputBox.AutoScroll = true;
 
 		Button okButton = new Button();
 		okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
@@ -79,7 +83,7 @@ public class RidderScript : CommandScript
 		//groep breedte
 		GroupBox groepbreed = new GroupBox();
 		groepbreed.Size = new System.Drawing.Size(180, 60);
-		groepbreed.Location = new System.Drawing.Point(10, 150);
+		groepbreed.Location = new System.Drawing.Point(225, 75);
 		groepbreed.Text = "Trap breedte";
 
 		Label breed = new Label();
@@ -110,7 +114,7 @@ public class RidderScript : CommandScript
 		//groep type
 		GroupBox groepBoxType = new GroupBox();
 		groepBoxType.Size = new System.Drawing.Size(395, 250);
-		groepBoxType.Location = new System.Drawing.Point(10, 225);
+		groepBoxType.Location = new System.Drawing.Point(10, 150);
 		groepBoxType.Text = "Trap type";
 
 
@@ -168,44 +172,61 @@ public class RidderScript : CommandScript
 		rbutton7.Size = new System.Drawing.Size(250, 25);
 		rbutton7.Location = new System.Drawing.Point(10, 200);
 		rbutton7.Checked = rb6;
-		rbutton7.Text = "Type 7";
+		rbutton7.Text = "Type 7 - (Niet in gebruik)";
 		groepBoxType.Controls.Add(rbutton7);
-		
-		
+
+
 
 		inputBox.Controls.Add(groepBoxType);
 
 		//groephoek	
 		GroupBox groepBoxHoek = new GroupBox();
-		groepBoxHoek.Size = new System.Drawing.Size(180, 100);
-		groepBoxHoek.Location = new System.Drawing.Point(10, 500);
+		groepBoxHoek.Size = new System.Drawing.Size(180, 175);
+		groepBoxHoek.Location = new System.Drawing.Point(10, 425);
 		groepBoxHoek.Text = "Trap hoek";
 
-
 		System.Windows.Forms.RadioButton rbutton10 = new RadioButton();
-		rbutton10.Size = new System.Drawing.Size(75, 25);
+		rbutton10.Size = new System.Drawing.Size(150, 25);
 		rbutton10.Location = new System.Drawing.Point(10, 25);
 		rbutton10.Checked = rb10;
-		rbutton10.Text = "42º";
+		rbutton10.Text = "42º    ^=210 >=225";
 		rbutton10.Checked = true;
 		groepBoxHoek.Controls.Add(rbutton10);
 
 		System.Windows.Forms.RadioButton rbutton11 = new RadioButton();
-		rbutton11.Size = new System.Drawing.Size(75, 25);
+		rbutton11.Size = new System.Drawing.Size(150, 25);
 		rbutton11.Location = new System.Drawing.Point(10, 50);
 		rbutton11.Checked = rb11;
-		rbutton11.Text = "37º";
+		rbutton11.Text = "37º    ^=190 >=250";
 		groepBoxHoek.Controls.Add(rbutton11);
+
+		System.Windows.Forms.RadioButton rbutton12 = new RadioButton();
+		rbutton12.Size = new System.Drawing.Size(150, 25);
+		rbutton12.Location = new System.Drawing.Point(10, 75);
+		rbutton12.Checked = rb12;
+		rbutton12.Text = "Afwijkend";
+		groepBoxHoek.Controls.Add(rbutton12);
+
+		System.Windows.Forms.TextBox textBox10 = new TextBox();
+		textBox10.Size = new System.Drawing.Size(130, 25);
+		textBox10.Location = new System.Drawing.Point(10, 100);
+		textBox10.Text = input10;
+		groepBoxHoek.Controls.Add(textBox10);
+
+		System.Windows.Forms.TextBox textBox11 = new TextBox();
+		textBox11.Size = new System.Drawing.Size(130, 25);
+		textBox11.Location = new System.Drawing.Point(10, 125);
+		textBox11.Text = input11;
+		groepBoxHoek.Controls.Add(textBox11);
 
 		inputBox.Controls.Add(groepBoxHoek);
 
 
 		//groepschoor	
 		GroupBox groepBoxSchoor = new GroupBox();
-		groepBoxSchoor.Size = new System.Drawing.Size(180, 100);
-		groepBoxSchoor.Location = new System.Drawing.Point(225, 500);
+		groepBoxSchoor.Size = new System.Drawing.Size(180, 175);
+		groepBoxSchoor.Location = new System.Drawing.Point(225, 425);
 		groepBoxSchoor.Text = "Schoor optie";
-
 
 		System.Windows.Forms.RadioButton rbutton20 = new RadioButton();
 		rbutton20.Size = new System.Drawing.Size(150, 25);
@@ -223,8 +244,76 @@ public class RidderScript : CommandScript
 		groepBoxSchoor.Controls.Add(rbutton21);
 
 		inputBox.Controls.Add(groepBoxSchoor);
-		
-		
+
+
+		//groepinloop	
+		GroupBox groepBoxInloop = new GroupBox();
+		groepBoxInloop.Size = new System.Drawing.Size(180, 150);
+		groepBoxInloop.Location = new System.Drawing.Point(10, 625);
+		groepBoxInloop.Text = "Inloop optie";
+
+		System.Windows.Forms.RadioButton rbutton30 = new RadioButton();
+		rbutton30.Size = new System.Drawing.Size(150, 25);
+		rbutton30.Location = new System.Drawing.Point(10, 25);
+		rbutton30.Checked = rb30;
+		rbutton30.Text = "Zonder horizontale inloop";
+		rbutton30.Checked = true;
+		groepBoxInloop.Controls.Add(rbutton30);
+
+		System.Windows.Forms.RadioButton rbutton31 = new RadioButton();
+		rbutton31.Size = new System.Drawing.Size(150, 25);
+		rbutton31.Location = new System.Drawing.Point(10, 50);
+		rbutton31.Checked = rb31;
+		rbutton31.Text = "Met horizontale inloop";
+		groepBoxInloop.Controls.Add(rbutton31);
+
+		System.Windows.Forms.TextBox textBox30 = new TextBox();
+		textBox30.Size = new System.Drawing.Size(130, 25);
+		textBox30.Location = new System.Drawing.Point(10, 75);
+		textBox30.Text = input30;
+		groepBoxInloop.Controls.Add(textBox30);
+
+		inputBox.Controls.Add(groepBoxInloop);
+
+
+		//groepuitloop	
+		GroupBox groepBoxUitloop = new GroupBox();
+		groepBoxUitloop.Size = new System.Drawing.Size(180, 150);
+		groepBoxUitloop.Location = new System.Drawing.Point(225, 625);
+		groepBoxUitloop.Text = "Uitloop optie";
+
+		System.Windows.Forms.RadioButton rbutton40 = new RadioButton();
+		rbutton40.Size = new System.Drawing.Size(150, 25);
+		rbutton40.Location = new System.Drawing.Point(10, 25);
+		rbutton40.Checked = rb40;
+		rbutton40.Text = "Zonder horizontale uitloop";
+		rbutton40.Checked = true;
+		groepBoxUitloop.Controls.Add(rbutton40);
+
+		System.Windows.Forms.RadioButton rbutton41 = new RadioButton();
+		rbutton41.Size = new System.Drawing.Size(150, 25);
+		rbutton41.Location = new System.Drawing.Point(10, 50);
+		rbutton41.Checked = rb41;
+		rbutton41.Text = "Met horizontale uitloop";
+		groepBoxUitloop.Controls.Add(rbutton41);
+
+		System.Windows.Forms.TextBox textBox40 = new TextBox();
+		textBox40.Size = new System.Drawing.Size(130, 25);
+		textBox40.Location = new System.Drawing.Point(10, 75);
+		textBox40.Text = input40;
+		groepBoxUitloop.Controls.Add(textBox40);
+
+
+
+		inputBox.Controls.Add(groepBoxUitloop);
+
+
+
+
+
+
+
+
 		inputBox.AcceptButton = okButton;
 		inputBox.CancelButton = cancelButton;
 
@@ -235,9 +324,20 @@ public class RidderScript : CommandScript
 
 		rb10 = rbutton10.Checked;
 		rb11 = rbutton11.Checked;
+		rb12 = rbutton11.Checked;
+		input10 = textBox10.Text;
+		input11 = textBox11.Text;
 
 		rb20 = rbutton20.Checked;
 		rb21 = rbutton21.Checked;
+
+		rb30 = rbutton30.Checked;
+		rb31 = rbutton31.Checked;
+		input30 = textBox30.Text;
+
+		rb40 = rbutton30.Checked;
+		rb41 = rbutton31.Checked;
+		input40 = textBox40.Text;
 
 		rb0 = rbutton0.Checked;
 		rb1 = rbutton1.Checked;
@@ -257,11 +357,23 @@ public class RidderScript : CommandScript
 		string input = "Aantal trappen";
 		decimal input1 = 1;
 		string input2 = "Trap breedte";
+
 		bool rb10 = false;
 		bool rb11 = false;
-		
+		bool rb12 = false;
+		string input10 = "Optrede";
+		string input11 = "Aantrede";
+
 		bool rb20 = false;
 		bool rb21 = false;
+
+		bool rb30 = false;
+		bool rb31 = false;
+		string input30 = "L in mm";
+
+		bool rb40 = false;
+		bool rb41 = false;
+		string input40 = "L in mm";
 
 		bool rb0 = true;
 		bool rb1 = false;
@@ -272,7 +384,12 @@ public class RidderScript : CommandScript
 		bool rb6 = false;
 		bool rb7 = false;
 
-		ShowInputDialog(ref input, ref input1, ref input2, ref rb10, ref rb11, ref rb20, ref rb21, ref rb0, ref rb1, ref rb2, ref rb3, ref rb4, ref rb5, ref rb6, ref rb7);
+		ShowInputDialog(ref input, ref input1, ref input2,
+							ref rb10, ref rb11, ref rb12, ref input10, ref input11,
+							ref rb20, ref rb21,
+							ref rb30, ref rb31, ref input30,
+							ref rb40, ref rb41, ref input40,
+							ref rb0, ref rb1, ref rb2, ref rb3, ref rb4, ref rb5, ref rb6, ref rb7);
 
 
 
@@ -282,6 +399,7 @@ public class RidderScript : CommandScript
 
 		string tredecode = "";
 		string supportcode = "";
+		string hoeklijncode = "";
 		string trapcode = "";
 		string trapcodeRH = "";
 		string trapcodeLH = "";
@@ -291,42 +409,98 @@ public class RidderScript : CommandScript
 		string bevessetsupplate = "S100338";
 		string traptype = "";
 		string Schoorlijst = "S100225";
+		decimal inloopB = 0;
+		decimal uitloopB = 0;
+		decimal inloopT = 0;
+		decimal uitloopT = 0;
+		decimal inloopL = 0;
+		decimal uitloopL = 0;
+
+		decimal hoog = input1;
+		decimal breed = Int32.Parse(input2);
+
+		decimal optreden = 0;
+		decimal aantreden = 0;
 
 		decimal treden = 0;
 		decimal optrede;
-		decimal hoog = input1;
 
-		decimal optreden42 = Math.Round(hoog / 210, 0);
-		decimal optreden37 = Math.Round(hoog / 190, 0);
-		decimal breed = Int32.Parse(input2);
+		decimal optreden42 = Math.Ceiling(hoog / 210);
+		decimal optreden37 = Math.Ceiling(hoog / 190);
 
 		if (rb10 == true)
 		{
+			optreden = 210;
+			aantreden = 225;
 			hoek = 42;
-			treden = optreden42 - 1;
-			optrede = optreden42;
+
+			optrede = Math.Ceiling(hoog / optreden);
+			treden = optrede - 1;
+
+			inloopB = 240;
+			uitloopB = 240;
 		}
 
 		else if (rb11 == true)
 		{
+			optreden = 190;
+			aantreden = 250;
 			hoek = 37;
-			treden = optreden37 - 1;
-			optrede = optreden37;
+
+			optrede = Math.Ceiling(hoog / optreden);
+			treden = optrede - 1;
+
+			inloopB = 270;
+			uitloopB = 270;
+		}
+
+		else if (rb12 == true)
+		{
+			optreden = Int32.Parse(input10);
+			aantreden = Int32.Parse(input11);
+			hoek = 42;
+
+			optrede = Math.Ceiling(hoog / optreden);
+			treden = optrede - 1;
+
+			inloopB = 270;
+			uitloopB = 270;
 		}
 
 		else
 		{
+			optreden = 0;
+			aantreden = 0;
 			hoek = 0;
+
 			treden = 0;
 			optrede = 0;
+
+			inloopB = 0;
+			uitloopB = 0;
 		}
 
 		double hoekrad = hoek * (Math.PI / 180);
 		double hoogd = Convert.ToDouble(hoog);
-
 		double lang = hoogd / (Math.Sin(hoekrad));
 
+		if (rb31 == true)
+		{
+			inloopL = Int32.Parse(input30);
+			inloopT = Math.Ceiling(inloopL / inloopB);
 
+			treden = treden + inloopT;
+			optrede = optrede + inloopT;
+		}
+
+		if (rb41 == true)
+		{
+			uitloopL = Int32.Parse(input40);
+			uitloopT = Math.Ceiling(uitloopL / uitloopB);
+
+			treden = treden + inloopT;
+			optrede = optrede + inloopT;
+		}
 
 
 		//Selecteren van de juiste trapboomset
@@ -477,14 +651,14 @@ public class RidderScript : CommandScript
 
 		if (hoek != 00)
 		{
-			if (breed == 600) { supportcode = "13750"; }
-			else if (breed == 650) { supportcode = ""; }
-			else if (breed == 700) { supportcode = ""; }
-			else if (breed == 750) { supportcode = "13752"; }
-			else if (breed == 800) { supportcode = "13453"; }
-			else if (breed == 900) { supportcode = "13452"; }
-			else if (breed == 1000) { supportcode = "13451"; }
-			else if (breed == 1200) { supportcode = "13450"; }
+			if (breed == 600) { supportcode = "13750"; hoeklijncode = ""; }
+			else if (breed == 650) { supportcode = ""; hoeklijncode = ""; }
+			else if (breed == 700) { supportcode = ""; hoeklijncode = ""; }
+			else if (breed == 750) { supportcode = "13752"; hoeklijncode = ""; }
+			else if (breed == 800) { supportcode = "13453"; hoeklijncode = "14266"; }
+			else if (breed == 900) { supportcode = "13452"; hoeklijncode = "14267"; }
+			else if (breed == 1000) { supportcode = "13451"; hoeklijncode = "14268"; }
+			else if (breed == 1200) { supportcode = "13450"; hoeklijncode = "14269"; }
 		}
 
 		if (type == 0) { ssm = 1; }
@@ -501,15 +675,11 @@ public class RidderScript : CommandScript
 		decimal tottrede = inputdec * treden;
 		decimal totsupp = inputdec * ssm;
 
-		decimal paintarea = 1;
-		int trapnummer = 1;
-		int coatingnummer = 1;
-
-
 		string trapcheck = trapcodeRH + " / " + trapcodeLH;
 		string trapcheck1 = trapcodeRH + trapcodeLH;
 
-		if (tredecode == "" || supportcode == "" || trapcheck1 == "" || bevessettrap == "" || inputdec == 0)
+
+		if (tredecode == "" || supportcode == "" || trapcheck1 == "" || bevessettrap == "" || inputdec == 0 || hoeklijncode == "")
 		{
 			MessageBox.Show("Er ontbreken artikelcodes");
 			MessageBox.Show("Stringer set = " + trapcheck +
@@ -532,6 +702,7 @@ public class RidderScript : CommandScript
 							"\n" +
 							"\nArtikelcode roostertrede: " + tredecode + " - " + tottrede + " x" +
 							"\nArtikelcode weltrede: " + supportcode + " - " + totsupp + " x" +
+							"\nArtikelcode hoeklijn: " + hoeklijncode + " - " + totsupp + " x" +
 							"\nArtikelcode trapboom RH: " + trapcodeRH + " - " + inputdec + " x" +
 							"\nArtikelcode trapboom LH: " + trapcodeLH + " - " + inputdec + " x"
 							, "Trebuchet");
@@ -566,6 +737,31 @@ public class RidderScript : CommandScript
 				if (rsItem != null && rsItem.RecordCount == 0)
 				{
 					MessageBox.Show("Geen overeenkomstig artikel kunnen vinden. Artikel: " + supportcode);
+				}
+				else
+				{
+					if (totsupp > 0)
+					{
+						ScriptRecordset rsAssemblyItem = this.GetRecordset("R_ASSEMBLYDETAILITEM", "", "PK_R_ASSEMBLYDETAILITEM= -1", "");
+						rsAssemblyItem.UseDataChanges = true;
+						rsAssemblyItem.AddNew();
+
+						rsAssemblyItem.Fields["FK_ASSEMBLY"].Value = this.FormDataAwareFunctions.CurrentRecord.GetPrimaryKeyValue();
+						rsAssemblyItem.Fields["FK_ITEM"].Value = rsItem.Fields["PK_R_ITEM"].Value;
+						rsAssemblyItem.Fields["QUANTITY"].Value = Convert.ToDouble(totsupp);
+
+						rsAssemblyItem.Update();
+					}
+				}
+			}
+
+			{   //hoeklijn invoegen
+				ScriptRecordset rsItem = this.GetRecordset("R_ITEM", "PK_R_ITEM, DESCRIPTION, CODE", string.Format("CODE = '{0}'", hoeklijncode), "");
+				rsItem.MoveFirst();
+
+				if (rsItem != null && rsItem.RecordCount == 0)
+				{
+					MessageBox.Show("Geen overeenkomstig artikel kunnen vinden. Artikel: " + hoeklijncode);
 				}
 				else
 				{
@@ -687,7 +883,6 @@ public class RidderScript : CommandScript
 				}
 				else
 				{
-
 					ScriptRecordset rsAssemblySub = this.GetRecordset("R_ASSEMBLYDETAILSUBASSEMBLY", "", "PK_R_ASSEMBLYDETAILSUBASSEMBLY= -1", "");
 					rsAssemblySub.UseDataChanges = true;
 					rsAssemblySub.AddNew();
@@ -729,7 +924,7 @@ public class RidderScript : CommandScript
 
 					}
 				}
-				
+
 			}
 
 
