acf3c31481e7d5b3ac03364e6cb03bb9a18e6678
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2025-02-11 16:02:22 +0100

    Update Default regels.cs

--- MeerMagazijn/TRA/Default regels.cs
+++ MeerMagazijn/TRA/Default regels.cs
@@ -117,7 +117,7 @@ public class RidderScript : CommandScript
 			RegelVuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
 			
 			//regel 12
-			taaknummer = xx;             //beklemming
+			taaknummer = 3;             //beklemming
 			risiconummer = 152;           //-----
 			maatregelnummer = 295;        //verwijder sierraden
 			RegelVuller(ref ordernummer, ref taaknummer, ref risiconummer, ref maatregelnummer, ref actiedoor);
